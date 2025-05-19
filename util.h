5c401c9f208528e17177aa67a6f95c68223b98e1
lidia-bozin <lidia.bozin@student.upt.ro>
2025-05-19 22:18:38 +0200

    Create util.h

---
+++ util.h
@@ -0,0 +1,14 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#define DIMENSIUNE 9
+#define TIMP_TOTAL 1200
+
+struct Jucator {
+    char nume[50];
+    int scor;
+};
+
+extern struct Jucator jucator;
+
+#endif
